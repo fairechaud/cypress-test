e47ec83ae8fe1a8b54639400d9a2e4fa3849b21c
fer <rodriguezcortes.fer@gmail.com>
2023-11-24 12:00:20 +1300

    Set the stage for the functionality test in dashboard/recruitment

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,23 @@
 describe('Login/authentication page', () => {
-  it.only('successfully takes demo user to dashboard', () => {
-    cy.loginSuccess()
-  })
-
+  
   it('fails to log in a fake user', () => 
   {
-    //same as cy.loginSuccess() but pass in fake credentials
+    // same as cy.loginSuccess() but pass in fake credentials
+  })
+  
+  it.only('successfully takes demo user to dashboard', () => {
+    cy.loginSuccess()
+    // assert that the dashboard page is shown, username shall
+    // be in display on the dropdown menu
   })
 
   it('displays the user in dropdown', () => {
-    
+    // would make for a good test if I can finish on time
   })
 })
+
+// Figure out a way to access cookies and maintain login session open, 
+// otherwise, just execute one test after the other to save time.
 
 context('Access the recruitment section', () => {
   it('verify stuff is displayed on screen', () => {
