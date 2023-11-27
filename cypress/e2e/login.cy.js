68dd2c0c16302ca1e2a7695edd13dea8c4b016b7
fer <rodriguezcortes.fer@gmail.com>
2023-11-27 13:48:04 +1300

    Completed login page test suite

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,15 +4,74 @@ describe('The authentication page', () => {
   
   it('fails to log in a fake user', () => 
   {
-    // same as cy.loginSuccess() but pass in fake credentials
-    cy.login('fakeUser',nonvalidLogin)
-    cy.url().should('include', nonvalidLogin)
+    // pass in fake credentials
+    cy.login('fakeUser')
+    cy.url().should('include', "login")
+  })
+
+  it('shows empty user span', () => 
+  {
+    cy.emptyUser()
+    cy.url().should('include', "login")
+    // alert displayed
+    cy.get('.oxd-input-group > .oxd-text')
+    .should("contain", "Required")
+  })
+
+  it('shows empty password span', () => 
+  {
+    cy.emptyPassword()
+    cy.url().should('include', "login")
+    // alert displayed
+    cy.get('.oxd-input-group > .oxd-text')
+    .should("contain", "Required")
+  })
+
+  it('lets user know password is wrong', () => 
+  {
+    cy.login('badPassword')
+    // shall not go beyond the login page
+    cy.url().should('include', "login")
+    // alert displayed
+    cy.get('div[role="alert"]')
+    .should("contain", "Invalid")
   })
   
-  it.only('logs Admin to dashboard', () => {
+  it("is able to reset password", () =>
+  {
+    cy.forgotPassword()
+    cy.url().should("include","Reset")
+    cy.get('h6').should("contain","Reset")
+    cy.get("input[placeholder='Username']")
+    .type("Admin")
+    cy.get('.oxd-button--secondary').click()
+    cy.get('h6').should("contain","successfully")
+  })
+
+  it('logs "Admin" to dashboard', () => {
     cy.login()
-    // assert that the dashboard page is shown, username shall
-    // be in display on the dropdown menu
+    // assert that the dashboard page is shown
+    cy.url()
+    .should('include', "dashboard")
+    // dropdown menu includes 'Logout' option
+    cy.get('.oxd-userdropdown-tab').click()
+    cy.get(".oxd-userdropdown-link")
+    .should("contain","Logout")
+  })
+
+  it('logs out succesfully', () => {
+    cy.login()
+    // assert that the dashboard page is shown
+    cy.url()
+    .should('include', "dashboard")
+    // multiple GET operations in place, 
+    // allocate enough time for these to finish
+    cy.wait(5000)
+    // dropdown menu includes 'Logout' option
+    cy.get('.oxd-userdropdown-tab').click()
+    cy.get('a:contains("Logout")').first().click()
+    // successfull logout
+    cy.url().should("include","login")
   })
 })
 
@@ -21,14 +80,14 @@ describe('The authentication page', () => {
 
 context('The recruitment section', () => {
   beforeEach(() => {
-    cy.autoLogin()
+    cy.login()
+    
   })
-  it.only('loads on screen', () => {
+  it('loads on screen', () => {
     cy.get('a:contains("Recruitment")')
       .first()
       .click()
-    cy.get('h6')
-      .should("contain","Recruitment")
+    cy.get('h6').should("contain", "Recruitment")
   })
 
   it('verify buttons are functional', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,26 +29,53 @@ const URL = "https://opensource-demo.orangehrmlive.com/"
 const userInput =  "input[name=username]"
 const pwInput = "input[name=password]"
 
-Cypress.Commands.add('login',(loginInfo = 'demoUser', page = "dashboard") =>
+Cypress.Commands.add('login',(loginInfo = 'demoUser') =>
 {
     cy.visit(URL)
-    // load credentials
     cy.fixture(loginInfo).as('userFixture')
     cy.get('@userFixture').then(user => {
         cy.get(userInput)
         .type(user.username)
         cy.get(pwInput)
         .type(user.password+'{enter}')
-        cy.url().should('include', page)
     })
     // if we had access to source code we could try to figure out 
-    // if the username is valid right now, the user's ifo is 
-    //randomized every now and then verify cookies here
+    // if the username is valid, the user's info is 
+    // randomized every now and then. Also how to verify cookies?
+})
+
+Cypress.Commands.add('emptyUser',(loginInfo = 'demoUser') =>
+{
+    cy.visit(URL)
+    cy.fixture(loginInfo).as('userFixture')
+    cy.get('@userFixture').then(user => {
+        cy.get(userInput)
+        .type(" ")
+        cy.get(pwInput)
+        .type(user.password+'{enter}')
+    })
+})
+
+Cypress.Commands.add('emptyPassword',(loginInfo = 'demoUser') =>
+{
+    cy.visit(URL)
+    cy.fixture(loginInfo).as('userFixture')
+    cy.get('@userFixture').then(user => {
+        cy.get(userInput)
+        .type(user.username)
+        cy.get(pwInput)
+        .type(" {enter}")
+    })
+})
+
+Cypress.Commands.add('forgotPassword', () => 
+{
+    cy.visit(URL)
+    cy.get('.orangehrm-login-forgot > .oxd-text').first().click()
 })
 
 Cypress.Commands.add('autoLogin',() =>
 {
-    
     cy.request({
         method: 'POST',
         url: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/validate',
@@ -59,11 +86,5 @@ Cypress.Commands.add('autoLogin',() =>
         }
     }).then((response) => {
         expect(response.status).to.equal(200)
-    })
-
-
-    
-    
-
-    
+    })   
 })
